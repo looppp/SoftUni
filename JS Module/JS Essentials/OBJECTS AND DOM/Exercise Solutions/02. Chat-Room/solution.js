58af94b54c989accaf6fbd86b9d5ab420da48428
Miroslav <cherybo@abv.bg>
2019-07-24 00:05:58 +0600

    commit

--- JS Module/JS Essentials/OBJECTS AND DOM/Exercise Solutions/02. Chat-Room/solution.js
+++ JS Module/JS Essentials/OBJECTS AND DOM/Exercise Solutions/02. Chat-Room/solution.js
@@ -1,5 +1,17 @@
 function solve() {
-   //TODO...
+    let targetDiv = document.getElementsByClassName('my-message')[0];
+    let sendButton = document.getElementById('send');
+    let inputTextArea = document.getElementById('chat_input');
+    let chatMessageArea = document.getElementById('chat_messages');
+
+    sendButton.addEventListener("click", function () {
+        let targetDivClone = targetDiv.cloneNode(true);
+        let textAreaContent = inputTextArea.value;
+        targetDivClone.textContent = textAreaContent;
+        chatMessageArea.appendChild(targetDivClone);
+
+        inputTextArea.value = "";
+    })
 }
 
 
