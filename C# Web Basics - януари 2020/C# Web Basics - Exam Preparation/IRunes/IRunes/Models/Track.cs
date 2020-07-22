5ae8b07639e02003f46e4dff872cfd94b9132f47
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2020-07-22 12:43:06 +0300

    Starting IRunes

---
+++ C# Web Basics - януари 2020/C# Web Basics - Exam Preparation/IRunes/IRunes/Models/Track.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+using System.Text;
+
+namespace IRunes.Models
+{
+    public class Track
+    {
+        public Track()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Link { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        //Navigational Property
+        public string AlbumId { get; set; }
+
+        public Album Album { get; set; }
+    }
+}
---
+++ C# Web Basics - януари 2020/C# Web Basics - Exam Preparation/IRunes/IRunes/Startup.cs
@@ -0,0 +1,21 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes
+{
+    public class Startup : IMvcApplication
+    {
+        public void Configure(IList<Route> routeTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
