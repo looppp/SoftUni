5486cc74cfb35cec03a53fad71b4886957046bc3
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2020-10-20 21:57:45 +0300

    exerciese solutions

--- JS Module/JS Advanced - September 2020/OBJECTS AND CLASSES/More Exercise/Rectangle.js
+++ JS Module/JS Advanced - September 2020/OBJECTS AND CLASSES/More Exercise/Rectangle.js
@@ -4,4 +4,15 @@ class Rectangle{
         this.height = Number(height);
         this.color = color;
     }
+
+
+    calcArea(){
+        return this.width * this.height;
+    }
 }
+
+let rect = new Rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
