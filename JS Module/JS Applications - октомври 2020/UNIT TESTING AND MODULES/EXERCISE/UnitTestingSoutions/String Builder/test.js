a1978ffce10816d53a380094891f657604a10236
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2020-11-04 21:00:52 +0200

    changes

--- JS Module/JS Applications - октомври 2020/UNIT TESTING AND MODULES/EXERCISE/UnitTestingSoutions/String Builder/test.js
+++ JS Module/JS Applications - октомври 2020/UNIT TESTING AND MODULES/EXERCISE/UnitTestingSoutions/String Builder/test.js
@@ -5,9 +5,12 @@ const StringBuilder = require('./stringBuilder');
 describe('StringBuilder', () => {
     let sb = new StringBuilder();
 
-    describe('append', () => {
-        it('shoud return an error if input isnt a string', () => {
-            assert.equal('Argument must be string', sb.append('53'));
+    describe('veryfyParam', () => {
+        it('shoud trown error when input is not string', () => {
+            assert.throws('Argument must be string', new StringBuilder(444));
         });
+        
     });
+
+    
 });
