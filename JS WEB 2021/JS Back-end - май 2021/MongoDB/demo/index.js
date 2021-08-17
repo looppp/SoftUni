f6106a2235649a710728b2200fc314817606bc7e
Miroslav Iliev <cherylooppp@gmail.com>
2021-08-17 21:49:16 +0300

    mongoDb demo

---
+++ JS WEB 2021/JS Back-end - май 2021/MongoDB/demo/index.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require ("mongodb");
+
+const connectionStr = 'mongodb://localhost:27017';
+
+const client = new MongoClient(connectionStr, {
+    useUnifiedTopology: true
+});
+client.connect(async (err) => {
+    if(err != null){
+        console.log('Something unexpected happened!');
+        return;
+    }
+
+    console.log('Database Connected');
+
+    const db = client.db('test');
+    const collection = db.collection('cats');
+    collection.insertOne({ name: "Gogata" });
+    const data = await collection.find({}).toArray();
+    console.log(data);
+});
