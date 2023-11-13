4990ef901b2f2a7086378b7b06726e47de23f479
Miroslav Iliev <cherybo@abv.bg>
2023-11-13 17:26:02 +0200

    TodoList demo and changes

---
+++ JS WEB 2023/ReactJS - октомври 2023/React Components Deep dive/Star Wars demo/src/App.jsx
@@ -0,0 +1,12 @@
+import Starwars from "./Starwars";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Starwars />
+    </div>
+  );
+}
+
+export default App;
---
+++ JS WEB 2023/ReactJS - октомври 2023/React Components Deep dive/Star Wars demo/src/Starwars.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function Starwars(props) {
+  const [characters, setCharacters] = useState([]);
+  console.log(characters);
+
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/people`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Star Wars Characters</h1>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.url}>{character.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
