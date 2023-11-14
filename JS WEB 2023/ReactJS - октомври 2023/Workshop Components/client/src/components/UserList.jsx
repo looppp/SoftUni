65caed1f2c1adbdb54e53e288670b1590fb98dd8
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2023-11-14 03:42:47 +0200

    Workshop components

---
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Components/client/src/components/UserList.jsx
@@ -0,0 +1,12 @@
+import Search from "./Search";
+import UserListTable from "./UserListTable";
+
+export default function UserList() {
+  return (
+    <section className="card users-container">
+      <Search />
+      <UserListTable />
+      <button className="btn-add btn">Add new user</button>
+    </section>
+  );
+}
