edae66c3bc74452bcca0ef47ba75ec41e614c965
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2023-11-15 04:22:13 +0200

    Deleting the old template and starting from scratch

--- JS WEB 2023/ReactJS - октомври 2023/YummyRecipes Project/client/src/App.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/YummyRecipes Project/client/src/App.jsx
@@ -1,16 +1,10 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import RecipesContainer from "./components/RecipesContainer";
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <main className="page">
-        <Header />
-        <RecipesContainer />
-      </main>
+      <Header />
       <Footer />
     </div>
   );
--- JS WEB 2023/ReactJS - октомври 2023/YummyRecipes Project/client/src/components/Header.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/YummyRecipes Project/client/src/components/Header.jsx
@@ -1,12 +1,17 @@
 export default function Header() {
   return (
-    <header className="hero">
-      <div className="hero-container">
-        <div className="hero-text">
-          <h1>Yummy Food</h1>
-          <h4>Recipes for everybody</h4>
-        </div>
-      </div>
+    <header className="header">
+      <a href="" className="logo">
+        Logo
+      </a>
+
+      <nav className="navbar">
+        <a href="">Home</a>
+        <a href="">Recipes</a>
+        <a href="">Login</a>
+        <a href="">Register</a>
+        <a href="">Logout</a>
+      </nav>
     </header>
   );
 }
