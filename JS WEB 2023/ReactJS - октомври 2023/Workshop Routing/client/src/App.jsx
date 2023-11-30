e6e3b7072a48c9ee8b0cf16729860ce88e0ddf7e
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2023-11-30 03:53:49 +0200

    Add game create page

--- JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import GameList from "./components/Game-list/GameList";
+import GameCreate from "./components/Game-create/GameCreate";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
       </Routes>
     </div>
   );
