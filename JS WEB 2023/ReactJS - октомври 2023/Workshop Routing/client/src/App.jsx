f08b84482f35f54ae5f759dbf0909c54afa47eaa
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2023-11-30 03:30:39 +0200

    Add header component and styles

--- JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <>
+    <div id="box">
+      <Header />
       <h1>Hello world</h1>
-    </>
+    </div>
   );
 }
 
