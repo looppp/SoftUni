160c89a1b95b2f3ce7849ced47f1555ce4b87345
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2023-12-01 03:38:53 +0200

    Add LoginKeys

--- JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
@@ -7,8 +7,15 @@ import GameCreate from "./components/Game-create/GameCreate";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Details from "./components/Details/Details";
+import { useState } from "react";
 
 function App() {
+  const [auth, setAuth] = useState({});
+
+  const loginSubmitHandler = (values) => {
+    console.log(values);
+  };
+
   return (
     <div id="box">
       <Header />
@@ -17,7 +24,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
         <Route path="/games/create" element={<GameCreate />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login loginSubmitHandler={loginSubmitHandler} />}
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/games/:gameId" element={<Details />} />
       </Routes>
--- JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/components/Login/Login.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/components/Login/Login.jsx
@@ -1,22 +1,45 @@
-export default function Login() {
+import useForm from "../../hooks/useForm";
+
+const loginFormKeys = {
+  Email: "email",
+  Password: "password",
+};
+
+export default function Login({ loginSubmitHandler }) {
+  const { values, onChange, onSubmit } = useForm(
+    {
+      [loginFormKeys.Email]: "",
+      [loginFormKeys.Password]: "",
+    },
+    loginSubmitHandler
+  );
+
   return (
-    <section id="login-page" class="auth">
-      <form id="login">
-        <div class="container">
-          <div class="brand-logo"></div>
+    <section id="login-page" className="auth">
+      <form id="login" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
           <h1>Login</h1>
           <label htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
-            name="email"
+            name={loginFormKeys.Email}
             placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values[loginFormKeys.Email]}
           />
 
           <label htmlFor="login-pass">Password:</label>
-          <input type="password" id="login-password" name="password" />
-          <input type="submit" class="btn submit" value="Login" />
-          <p class="field">
+          <input
+            type="password"
+            id="login-password"
+            name={loginFormKeys.Password}
+            onChange={onChange}
+            value={values[loginFormKeys.Password]}
+          />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
             <span>
               If you don't have profile click <a href="#">here</a>
             </span>
