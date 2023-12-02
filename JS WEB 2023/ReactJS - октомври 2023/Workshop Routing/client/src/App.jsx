f9c40ee33d2eb3d12fdf29d2544508128a111f50
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2023-12-02 19:35:43 +0200

    CHange register page to controlled

--- JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import * as authService from "./services/authService";
 import AuthContext from "./contexts/authContext";
+import Path from "./paths";
 
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
@@ -13,16 +14,31 @@ import Register from "./components/Register/Register";
 import Details from "./components/Details/Details";
 
 function App() {
+  const navigate = useNavigate();
   const [auth, setAuth] = useState({});
 
   const loginSubmitHandler = async (values) => {
     const result = await authService.login(values.email, values.password);
 
-    console.log(result);
+    setAuth(result);
+
+    navigate(Path.Home);
+  };
+
+  const registerSubmitHandler = async (values) => {
+    console.log(values);
+  };
+
+  const values = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    username: auth.username,
+    email: auth.email,
+    isAuthenticated: !!auth.username,
   };
 
   return (
-    <AuthContext.Provider value={{ loginSubmitHandler }}>
+    <AuthContext.Provider value={values}>
       <div id="box">
         <Header />
 
--- JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/components/Register/Register.jsx
+++ JS WEB 2023/ReactJS - октомври 2023/Workshop Routing/client/src/components/Register/Register.jsx
@@ -1,9 +1,31 @@
+import { useContext } from "react";
+
+import AuthContext from "../../contexts/authContext";
+import useForm from "../../hooks/useForm";
+
+const registerFormKeys = {
+  Email: "email",
+  Password: "password",
+  ConfirmPassword: "confirm-password",
+};
+
 export default function Register() {
+  const { registerSubmitHandler } = useContext(AuthContext);
+
+  const { values, onChange, onSubmit } = useForm(
+    {
+      [registerFormKeys.Email]: "",
+      [registerFormKeys.Password]: "",
+      [registerFormKeys.ConfirmPassword]: "",
+    },
+    registerSubmitHandler
+  );
+
   return (
-    <section id="register-page" class="content auth">
-      <form id="register">
-        <div class="container">
-          <div class="brand-logo"></div>
+    <section id="register-page" className="content auth">
+      <form id="register" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
           <h1>Register</h1>
 
           <label htmlFor="email">Email:</label>
@@ -12,21 +34,31 @@ export default function Register() {
             id="email"
             name="email"
             placeholder="maria@email.com"
+            onChange={onChange}
+            value={values[registerFormKeys.Email]}
           />
 
           <label htmlFor="pass">Password:</label>
-          <input type="password" name="password" id="register-password" />
+          <input
+            type="password"
+            name="password"
+            id="register-password"
+            onChange={onChange}
+            value={values[registerFormKeys.Password]}
+          />
 
           <label htmlFor="con-pass">Confirm Password:</label>
           <input
             type="password"
             name="confirm-password"
             id="confirm-password"
+            onChange={onChange}
+            value={values[registerFormKeys.ConfirmPassword]}
           />
 
-          <input class="btn submit" type="submit" value="Register" />
+          <input className="btn submit" type="submit" value="Register" />
 
-          <p class="field">
+          <p className="field">
             <span>
               If you already have profile click <a href="#">here</a>
             </span>
