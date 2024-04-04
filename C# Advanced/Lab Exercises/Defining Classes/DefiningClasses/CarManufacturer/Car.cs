4bda6bb95bc54f4839f67e8cfa4b3c61d7f95de1
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-04-04 04:27:15 +0300

    Defining classes Lab

---
+++ C# Advanced/Lab Exercises/Defining Classes/DefiningClasses/CarManufacturer/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            fuelQuantity = 200;
+            fuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelConsumption, double fuelQuantity) : this(make, model, year)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+        public double FuelQuantity { get => fuelQuantity; set => fuelQuantity = value; }
+        public double FuelConsumption { get => fuelConsumption; set => fuelConsumption = value; }
+        public Engine Engine { get => engine; set => engine = value; }
+        public Tire[] Tires { get => tires; set => tires = value; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - (FuelConsumption * distance) > 0)
+            {
+                FuelQuantity -= FuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmi()
+        {
+
+            string output = $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:F2}";
+
+            return output;
+        }
+
+
+    
+    }
+}
