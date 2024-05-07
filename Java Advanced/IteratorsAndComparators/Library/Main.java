7fec6911b27b089ac2aac37d005433991df78e08
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-05-07 12:28:31 +0300

    Custom Book Library

--- Java Advanced/IteratorsAndComparators/Library/Main.java
+++ Java Advanced/IteratorsAndComparators/Library/Main.java
@@ -2,6 +2,8 @@ package IteratorsAndComparators.Library;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookTwo = new Book("The Documents in the case", 1930, "Dorothy Sayers", "Robert Eustace");
+        Book bookThree = new Book("The Documents in the case", 2002);
     }
 }
