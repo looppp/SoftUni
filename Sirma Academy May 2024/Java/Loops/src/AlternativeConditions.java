25efe3d5ef5ae73dd7784958c82b0e53dd1c171b
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-05-26 22:47:23 +0300

    HourGlass from Loops Done

--- Sirma Academy May 2024/Java/Loops/src/AlternativeConditions.java
+++ Sirma Academy May 2024/Java/Loops/src/AlternativeConditions.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class AlternativeConditions {
     public static void main(String[] args) {
-        //TODO finish the logic to match the result
         Scanner sc = new Scanner(System.in);
         int input = Integer.parseInt(sc.nextLine());
         int numberToPrint = 1;
-
+        int emptySlots = input / 2;
+//TODO implement empty space logic or something else(emptySlots*)
         for (int i = 0; i < input; i++) {
+            System.out.printf("%s", " ".repeat(emptySlots--));
             for (int j = 0; j <= i && numberToPrint <= input; j++) {
-                System.out.printf("%d ", numberToPrint++);
+                System.out.printf(" %d", numberToPrint++);
             }
             System.out.println();
+
         }
     }
 }
--- Sirma Academy May 2024/Java/Loops/src/HourglassPattern.java
+++ Sirma Academy May 2024/Java/Loops/src/HourglassPattern.java
@@ -4,30 +4,33 @@ public class HourglassPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-//TODO FINISH IT!
-        for (int i = 0; i < n / 2; i++) {
-            System.out.print(" ".repeat(i));
-            if (i == 0) {
-                System.out.println("#".repeat(n + 2));
-            } else {
-                System.out.print("#");
-                int spaces = (n - 1) - 2 + i ;
-                if (spaces >= 0) {
-                    System.out.print(" ".repeat(spaces));
-                    System.out.println("#");
-                } else {
-                    System.out.println();
-                }
+
+        System.out.printf("%s%n", "#".repeat(n + 2));
+        int counter = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 2 == 0) {
+                System.out.printf("%s#", " ".repeat(counter++));
+                System.out.printf("%s", " ".repeat(n - 2 - i));
+                System.out.printf("#%n");
             }
         }
 
-        if(n % 2 == 0){
-            System.out.printf("%s%s", " ".repeat(n/ 2 + 3), "##");
+        if(n % 2 != 0){
+            System.out.printf("%s%s%n", " ".repeat(n / 2), " #");
+            counter = n / 2;
         } else {
-            System.out.printf("%s%s", " ".repeat(n / 2), "#");
-
+            counter = n / 2 - 1;
         }
 
+        for (int i = n - 3; i >= 0 ; i--) {
+            if (i % 2 == 0) {
+                System.out.printf("%s#", " ".repeat(counter--));
+                System.out.printf("%s", " ".repeat(n - 2 - i));
+                System.out.printf("#%n");
+            }
+        }
+        System.out.printf("%s%n", "#".repeat(n + 2));
 
     }
 }
